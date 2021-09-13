adecf6f1a9c3462427f88f994c7037c8e3ec6f31
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2021-09-13 20:37:50 +0300

    Add files via upload

---
+++ KG/kindergarten/photos/old/js/start.js
@@ -0,0 +1,33 @@
+ 
+
+window.onload = function () {
+    lax.init()
+    let clientH = document.querySelector('body');
+    console.log(clientH.scrollHeight);
+  
+    // Add a driver that we use to control our animations
+    lax.addDriver('scrollY', function () {
+        return window.scrollY
+      }, { inertiaEnabled: true })
+
+    // Add animation bindings to elements
+     
+    
+    lax.addElements(
+      '.anim1',  // Element selector rule
+      {             // Animation data
+        scrollY: {  
+          
+          scale: [
+            [0,'elCenterY','elOutY'],
+            [0.8, 1.1, 0.3]
+          ],
+        
+        }
+      },
+      {             
+        style: {}   // Element options
+      })
+
+
+  }
