b0eee98c0df0980c8824779065d6ae1f833579fd
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2022-06-09 21:09:57 +0300

    Add files via upload

--- sw/service-worker.js
+++ sw/service-worker.js
@@ -1,6 +1,19 @@
-self.addEventListener('install', function(e) {
-    console.log('Install Event:', e);
+self.addEventListener('install',async function(e) {
+    let options= {
+        headers:{"Content-type":"application/json;charset=utf-8"},
+        body: JSON.stringify({inst_evt:e}),
+        method:"post"
+    }
+    //await fetch('https://localhost:3000/w',options)
+    console.log(options)
+    
     });
-    self.addEventListener('activate', function(e) {
-    console.log('Activate Event:', e);
-    });
+    self.addEventListener('activate', async function(e) {
+        let options= {
+            headers:{"Content-type":"application/json;charset=utf-8"},
+            body: JSON.stringify({activ_evt:e}),
+            method:"post"
+        }
+        //await fetch('https://localhost:3000/w',options)
+        console.log(options);
+        });
